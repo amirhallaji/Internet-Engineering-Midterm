e45efbe0e56c46f06875b1319668462e8078ca52
amirhallaji <a.hallaji.b@gmail.com>
2021-04-14 21:47:37 +0430

    simple fetch is done.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 function getGender() {
-    
+    var res = document.getElementById("name_input").value;
+    fetch(`https://api.genderize.io/?name=${res}`)
+        .then(res => res.json())
+        .then(data => {
+            let name = data.name;
+            if (data.gender == null) {
+                window.alert("Error");
+            }
+            else {
+                let gender = data.gender;
+                let probability = data.probability;
+                document.getElementById("prediction-gender").innerHTML = gender;
+                document.getElementById("prediction-probability").innerHTML = probability;
+            }
+        });
+
+
 }
+
+// window.localStorage.
